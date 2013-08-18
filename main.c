5c43be5417d5cce83dd44f1977aad519986b2175
John Luk <john.37@gmail.com>
2013-08-18 17:56:46 +0800

    add point movement function

--- main.c
+++ main.c
@@ -1,15 +1,55 @@
 #include "/usr/include/ncursesw/ncurses.h"
 
+int maxy, maxx;
+void decrease(int *num) {
+	if (*num > 0) {
+		(*num)--;
+	}
+}
+
+void increase(int *num) {
+	if (*num < maxy - 1) {
+		(*num)++;
+	}
+}
+
+void get_maxyx() {
+	getmaxyx(stdscr, maxx, maxy);
+	mvprintw(0, 0, "maxx:%d, maxy:%d\n", maxx, maxy);
+}
+
 int main(void)
 {
+	WINDOW* win;
 	initscr();
+	curs_set(FALSE);
+	noecho();
+	get_maxyx();
+	
 	int key = 0;
 	int row = 0;
-	while ((char)key != 'k'){
+	int col = 0;
+	while ((char)key != 'q'){
 		key = getch();
-		mvprintw(row, 0, "Entered = %x", key);
+		switch(key) {
+		case 'h':
+			decrease(&col);
+			break;
+		case 'j':
+			increase(&row);
+			break;
+		case 'k':
+			decrease(&row);
+			break;
+		case 'l':
+			increase(&col);
+			break;
+		default:
+			continue;
+		}
+		wclear(stdscr);
+		mvprintw(row, col, "#");
 		refresh();
-		row++;
 	}
 	endwin();
 	return 0;
